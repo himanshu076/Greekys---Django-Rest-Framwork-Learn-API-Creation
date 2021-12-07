76c3275d6ac1ff8a8ec9c646713c768cb553e9e7
himanshu <himanshu.andel@yahoo.in>
2021-12-08 01:21:49 +0530

    Created Class based APIview 12

--- 12_CURD_ClassBasedAPIView/api/views.py
+++ 12_CURD_ClassBasedAPIView/api/views.py
@@ -1,13 +1,12 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import status
+from rest_framework.views import APIView
 
 # Create your views here.
-@api_view(['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
-def student_api(request, pk=None):
-    if request.method == 'GET':
+class StudentAPI(APIView):
+    def get(self, request, pk=None, format=None):
         id = pk
         if id is not None:
             stu = Student.objects.get(pk=id)
@@ -17,14 +16,14 @@ def student_api(request, pk=None):
         serializer = StudentSerializer(stu, many=True)
         return Response(serializer.data)
 
-    if request.method == 'POST':
+    def post(self, request, format=None):
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({'msg':'Data Created'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'PUT':
+    def put(self, request, pk, format=None):
         id = pk
         stu = Student.objects.get(pk=id)
         serializer = StudentSerializer(stu, data=request.data)
@@ -33,7 +32,7 @@ def student_api(request, pk=None):
             return Response({'msg': 'Complete Data Updated'})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'PATCH':
+    def patch(self, request, pk, format=None):
         id = pk
         stu = Student.objects.get(pk=id)
         serializer = StudentSerializer(stu, data=request.data, partial=True)
@@ -42,7 +41,7 @@ def student_api(request, pk=None):
             return Response({'msg': 'Partial Data Updated'})
         return Response(serializer.errors)
 
-    if request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
         id = pk
         stu = Student.objects.get(pk=id)
         stu.delete()
